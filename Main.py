e9642308ac400bd87193a1335e782a84b0372248
Olivier Braibant <olivier.braibant@student.uclouvain.be>
2022-11-19 15:50:12 +0100

    Starting with clean import

---
+++ Main.py
@@ -0,0 +1,15 @@
+import pandas_datareader as pdr
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+
+start = dt.datetime(2020, 1, 1)
+stop = dt.datetime(2021, 1, 1)
+data_df = pdr.get_data_yahoo("MSFT", start, stop)
+arr = data_df.to_numpy()
+
+print(data_df.head())
+print("Moyenne Low: ", data_df["Low"].mean(), "  ",
+      "Moyenne High: ", data_df["High"].mean()
+      )
