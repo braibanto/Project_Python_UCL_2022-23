a9cf889d9b5c299673e685848d045534ae947b71
Olivier Braibant <olivier.braibant@student.uclouvain.be>
2022-11-20 17:40:15 +0100

    Cleaning

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import math
 import scipy.optimize as scy
 import numpy as np
 import pandas as pd
@@ -40,13 +39,14 @@ def fetch_stock_returns(stocks, start_date, end_date):
     return moy_return, matrix_cov
 
 
-def ponderation_random_stardardized(stocks):
-    """créer un np.array de pondérations aléatoires standardisées (somme = 100%)
-    On utilise à cette fin une générateur aléatoire [0,1]
+def ponderation_random_stardardised(stocks):
+
+    """ Crée un np.array de pondérations aléatoires standardisées (somme = 100%)
+    On utilise à cette fin un générateur de nombre aléatoire [0,1]
     INPUT:
         - stocks: liste des actions à pondérer
     OUTPUT:
-        - poids_std: np.array contenant les pondérations aléatoires standardiséés """
+        - poids: np.array contenant les pondérations aléatoires standardisées """
 
     poids = np.random.random(len(stocks))
     poids /= np.sum(poids)
@@ -58,7 +58,7 @@ def perf_porfolio(poids, moy_return, cov_matrix):
     """ Calcul la performance et le risque du portefeuille en tenant de pondération individuelle des action
      INPUT:
         - poids: dataframe contenant les pondérations du portefeuille
-        - moy_return: datafram contenant les moyennes journalières des actions
+        - moy_return: dataframe contenant les moyennes journalières des actions
         - cov_matrix: dataframe contenant la matrice var/cov des returns journaliers
     OUTPUT:
         - return_portfolio: un float égale au return annuel pondéré du portefeuille
@@ -70,7 +70,7 @@ def perf_porfolio(poids, moy_return, cov_matrix):
 
 
 def sharp_ratio_opp(poids, stock_return, cov_matrix, taux_ss_risque=0):
-    """ Calcul du Sharp ratio (négatif! - voir scipy) d'un portefeuille pour une pondération donnée
+    """ Calcul du Sharp ratio (négatif ! - voir scipy) d'un portefeuille pour une pondération donnée
     INPUT:
         - poids: np.array contenant la pondération des actions
         - stock_return: array contenant la moyenne """
@@ -88,12 +88,10 @@ start_d = dt.datetime(2020, 1, 1)
 stop_d = dt.datetime(2021, 1, 31)
 
 ret_stocks, cov_mat = fetch_stock_returns(stock_list, start_d, stop_d)
-poids_port = ponderation_random_stardardized(stock_list)
+poids_port = ponderation_random_stardardised(stock_list)
 return_portefeuille, risk_portefeuille = perf_porfolio(poids_port, ret_stocks, cov_mat)
 
 print(poids_port)
 print("Check poids = ", sum(poids_port)*100, " %")
-print("Return_porfolio: ", return_portefeuille * 100)
+print("Return portfolio: ", return_portefeuille * 100)
 print("Risk portfolio: ", risk_portefeuille * 100)
-
-
